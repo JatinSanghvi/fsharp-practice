dc96bed67bbb30816c7c2ce2dbd3e9e2ac046f18
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2017-10-02 22:04:57 +0530

    Add HackerRank Warmup problem solutions

---
+++ src/HackerRank/Warmup/001_SolveMeFirst.fsx
@@ -0,0 +1,30 @@
+﻿(*
+Solve Me First
+
+Welcome to HackerRank! The purpose of this challenge is to familiarize you with reading input from stdin (the standard
+input stream) and writing output to stdout (the standard output stream) using our environment.
+
+Review the code provided in the editor below, then complete the solveMeFirst function so that it returns the sum of two
+integers read from stdin. Take some time to understand this code so you're prepared to write it yourself in future
+challenges.
+
+Select a language below, and start coding!
+
+Input Format
+
+Code that reads input from stdin is provided for you in the editor. There are  lines of input, and each line contains a
+single integer.
+
+Output Format
+
+Code that prints the sum calculated and returned by solveMeFirst is provided for you in the editor.
+*)
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    let num1 = Console.ReadLine() |> int
+    let num2 = Console.ReadLine() |> int
+    printfn "%d" (num1 + num2)
+    0
---
+++ src/HackerRank/Warmup/005_DiagonalDifference.fsx
@@ -0,0 +1,74 @@
+﻿(*
+Diagonal Difference
+
+Given a square matrix of size N x N, calculate the absolute difference between the sums of its diagonals.
+
+Input Format
+
+The first line contains a single integer, N. The next N lines denote the matrix's rows, with each line containing N space-
+separated integers describing the columns.
+
+Constraints
+
+-100 <= Elements in the matrix <= 100
+
+Output Format
+
+Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+
+Sample Output
+
+15
+
+Explanation
+
+The primary diagonal is:
+
+11
+   5
+     -12
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+
+     4
+   5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19 
+Difference: |4 - 19| = 15
+
+Note: |x| is absolute value function
+*)
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    let size = Console.ReadLine() |> int
+    let matrix = List.init size (fun _ -> Console.ReadLine().Split [|' '|] |> Array.toList |> List.map int)
+
+    let rec diagonal_sum matrix =
+        let sub_matrix matrix =
+            match matrix with
+            | row :: rows -> List.map(fun row ->
+                match row with
+                | cell :: cells -> cells
+                | _ -> raise (new InvalidOperationException("Invalid row"))) rows
+            | _ -> raise (new InvalidOperationException("Invalid matrix"))
+
+        match matrix with
+        | [] -> 0
+        | [[cell]] -> cell
+        | (cell :: _) :: rows -> cell + diagonal_sum (sub_matrix matrix)
+        | _ -> raise (new InvalidOperationException("Invalid matrix"))
+
+    let diagonal_difference = abs ((diagonal_sum matrix) - (diagonal_sum (List.rev matrix)))
+    printfn "%d" diagonal_difference
+    0
