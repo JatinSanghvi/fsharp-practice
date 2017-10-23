f9cfd33de87b4b0f8adb00f6a769059acf7067e6
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2017-10-23 22:10:40 +0530

    Add HackerRank Warmup problem solutions

---
+++ src/HackerRank/Warmup/009_BirthdayCakeCandles.fsx
@@ -0,0 +1,48 @@
+﻿(*
+Birthday Cake Candles
+
+Colleen is turning n years old! Therefore, she has n candles of various heights on her cake, and candle i has height
+height_i. Because the taller candles tower over the shorter ones, Colleen can only blow out the tallest candles.
+
+Given the height_i for each individual candle, find and print the number of candles she can successfully blow out.
+
+Input Format
+
+The first line contains a single integer, n, denoting the number of candles on the cake. 
+The second line contains n space-separated integers, where each integer i describes the height of candle i.
+
+Constraints
+
+1 <= n <= 10^5
+
+Output Format
+
+Print the number of candles Colleen blows out on a new line.
+
+Sample Input 0
+
+4
+3 2 1 3
+
+Sample Output 0
+
+2
+
+Explanation
+
+We have one candle of height 1, one candle of height 2, and two candles of height 3. Colleen only blows out the tallest
+candles, meaning the candles where height = 3. Because there are 2 such candles, we print 2 on a new line.
+*)
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    Console.ReadLine() |> ignore
+
+    let candleHeights = Console.ReadLine().Split [|' '|] |> Array.map int
+    let tallestHeight = Array.max candleHeights
+    let tallestCandles = candleHeights |> Array.filter (fun ch -> ch = tallestHeight) |> Array.length
+
+    printfn "%d" tallestCandles
+    0
---
+++ src/HackerRank/Warmup/010_TimeConversion.fsx
@@ -0,0 +1,35 @@
+﻿(*
+Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a 12-hour clock,
+and 12:00:00 on a 24-hour clock.
+
+Input Format
+
+A single string containing a time in 12-hour clock format (i.e.: hh:mm:ssAM or hh:mm:ssPM), where 01 <= hh <= 12 and
+00 <= mm <= 59.
+
+Output Format
+
+Convert and print the given time in 24-hour format, where 00 <= hh <= 23.
+
+Sample Input
+
+07:05:45PM
+
+Sample Output
+
+19:05:45
+*)
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    let time12 = Console.ReadLine();
+
+    let hour = (time12.Substring(0, 2) |> int) % 12;
+    let hour = if time12.[8] = 'A' then hour else hour + 12;
+
+    printfn "%02d%s" hour (time12.Substring(2, 6))
+    0
